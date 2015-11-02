e24b77b0fbc56163e90993451079f52f14d55528
Jacob Cole <jacob@Jacobs-MacBook-Air.local>
2015-11-01 20:42:39 -0800

    challenge3

---
+++ challenge3.js
@@ -0,0 +1,22 @@
+
+// console.log("hi")
+// process.stdout.write("aasdfsdf" + '\n');
+
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  process.stdout.write("Enter your name: ");
+
+  if (chunk !== null) {
+    process.stdout.write('Hi, ' + chunk + "!");
+  }
+});
+
+process.stdin.on('end', function() {
+  process.stdout.write('end');
+});
+
+// var fs = require("fs")
+// fs.createReadStream(process.argv[2]).pipe(process.stdout)
